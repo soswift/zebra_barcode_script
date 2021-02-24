249aaed091a9a5bc5fc06e139de7ae6fbafd44e1
soswift <swifts@hawaii.edu>
2021-02-23 14:15:21 -1000

    updated template,barcode replacement, started log

--- barcode_gen_script.R
+++ barcode_gen_script.R
@@ -5,33 +5,40 @@
 # set working directory
 setwd("/home/sean/Documents/LabelFormats/zebra")
 
-
-# Read in ZPL template
-zpl_raw <-readLines(con = "SW_tube_dot_label.zpl")
-zpl_edit <- zpl
-
+# Define files and parameters
 # start off barcode number
 x = 1000001
+template_bcode = "0000000"
 outfile = "output.zpl"
-barcode_n = 5
+barcode_n = 500
+zpl_file = "SW_tube_dot_label_v2.zpl"
+barcode_log_file = "barcode_print_log.txt"
 
+# Read in ZPL template
+zpl_raw <-readLines(con = zpl_file)
 # Overwrite output file if it exists
 if(file.exists(outfile)){
 file.remove(outfile)
+print("removing old print file")
 }
 
+# loop through new barcodes
+print("adding new barcodes to print file:")
 for(a_barcode in c(x:(x+barcode_n))){
-# Replace the barcode parts with new numbers
-# Tube barcode
-zpl_edit[26] <- sub("0000000", a_barcode, zpl_raw[26])
+	# Replace the barcode parts with new numbers
+	zpl_edit <- sub(template_bcode, a_barcode, zpl_raw)
+	print(a_barcode)
+	# write print instructions to file
+	sink(file = outfile, append = T)
+	cat(zpl_edit, sep = "\n")
+	sink()
 
-# Cap barcode
-zpl_edit[28] <- sub("0000000", a_barcode, zpl_raw[28])
-
-# write to file
-sink(file = outfile, append = T)
-cat(zpl_edit, sep = "\n")
-sink()
+	# write barcodes printed to log
+	sink(file=barcode_log_file, append = T)
+	print(paste(a_barcode,
+		    date(),
+		    zpl_file,
+		    sep = ","))
+	sink()
 }
-
 
